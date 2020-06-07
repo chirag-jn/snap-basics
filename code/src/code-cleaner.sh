965f71d406e242df252f5d62e1124a7c9cba0dc5
Chirag Jain <jainchg@gmail.com>
2020-06-07 22:20:37 +0000

    Shared Object file building

---
+++ code/src/code-cleaner.sh
@@ -0,0 +1,14 @@
+rm aclocal.m4
+rm ar-lib
+rm -r autom4te.cache
+rm compile
+rm config.*
+rm configure
+rm depcomp
+rm install-sh
+rm libtool
+rm ltmain.sh
+rm Makefile
+rm Makefile.in
+rm missing
+rm stamp-h1
--- parts/basic-tut/build/src/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-
-int main(int argc, char *argv[]) {
-	puts("Hello World! What's up");
-	return 0;
-}
